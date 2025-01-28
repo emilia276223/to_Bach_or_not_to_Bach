e4df8e75e1c662edaeb1fe2940661fc28f482549
Kalina Białek <118077983+KalBia@users.noreply.github.com>
2025-01-28 09:33:51 +0100

    comment fix in knn.py

--- models/knn.py
+++ models/knn.py
@@ -5,7 +5,7 @@ class KNN(BaseModel):
     Takes:
         optionally n_neighbors - at how many neighbors should KNN look (default = 3)
         optionally weights - "uniform" is default, the other option is "distance"
-    Calculates: X_train, X_test, Y_train, Y_test, model
+    Calculates: model
     '''
     def fit(self, n_neighbors=7, weights="distance"):
         self.model = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
